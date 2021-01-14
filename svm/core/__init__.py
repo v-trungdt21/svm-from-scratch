c5e3280977d9d34bb841f3f63a33e4e03ce1cca7
trungdt21 <v.trungdt21@vinai.io>
2021-01-14 16:47:03 +0700

    Improve SVM init

--- svm/core/__init__.py
+++ svm/core/__init__.py
@@ -1,1 +1,2 @@
+from .kernels import get_kernel_function
 from .svm import SVM
--- svm/core/svm.py
+++ svm/core/svm.py
@@ -2,17 +2,32 @@ import numpy as np
 
 from svm.utils import BaseException
 
+from .kernels import get_kernel_function
+
 
 class SVM:
-    def __init__(self):
-        pass
+    def __init__(
+        self, kernel="linear", C=None, degree=2.0, gamma=5.0, coef=1.0
+    ):
+        """Initialize SVM model
+        kernel: Specifies the kernel type to be used in the algorithm.
+        C: Regularization parameter. If None, use linear SVM.
+        degree: Degree of the polynomial kernel function (‘poly’). Ignored by all other kernels.
+        gamma: Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’.
+        coef: Independent term in kernel function. It is only significant in ‘poly’ and ‘sigmoid’.
+        """
+        self.kernel = get_kernel_function(
+            kernel, degree=degree, gamma=gamma, coef=coef
+        )
+        self.C = C
+        if self.C is not None:
+            self.C = float(self.C)
 
     def fit(self, data):
         pass
 
     def predict(self, features):
-        """
-        Infer feature set after training.
+        """Infer feature set after training.
         features: np.array: data you need to infer.
         output: {-1,1}: label.
         """
