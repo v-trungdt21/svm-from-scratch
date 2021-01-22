80a829decd31ceb18f04778066d0369002ce7a8a
namtranvinai <v.namtd12@vinai.io>
2021-01-22 21:42:23 +0700

    update test kernel function

--- svm/tests/test_svm_kenel.py
+++ svm/tests/test_svm_kenel.py
@@ -19,7 +19,8 @@ def test_speed_linear():
 
     logging.debug("Test speed for svm kernel linear.")
     start = timeit.default_timer()
-    kf_result = partial(kernels.get_kernel_function, kernel="linear", x=x, z=z)
+    kf_linear = kernels.get_kernel_function(kernel="linear")
+    kf_result = kf_linear(x=x, z=z)
     stop = timeit.default_timer()
     logging.debug("Time consump by svm kernel linear: %s", stop - start)
 
@@ -31,7 +32,7 @@ def test_speed_linear():
     logging.debug("kf_result: %s, direct_result: %s", kf_result, direct_result)
 
     if kf_result != direct_result:
-        return BaseException("False")
+        raise BaseException("False")
 
 
 def test_speed_poly():
@@ -40,10 +41,30 @@ def test_speed_poly():
 
 
 def test_speed_rbf():
-    """Updating..."""
-    pass
+    """Test speed (check) for svm kernel rbf scaler vs math linear kernel."""
+    x = np.random.randint(100, size=(100000))
+    z = np.random.randint(100, size=(100000))
+
+    logging.debug("Test speed for svm kernel rbf.")
+    start = timeit.default_timer()
+    kf_linear = kernels.get_kernel_function(kernel="rbf", gamma=1.00)
+    kf_result = kf_linear(x=x, z=z)
+    logging.debug("Value of svm kernel rbf scale: %s", kf_result)
+    stop = timeit.default_timer()
+    logging.debug("Time consump by svm kernel rbf scale: %s", stop - start)
+
+    start = timeit.default_timer()
+    kf_linear = kernels.get_kernel_function(kernel="rbf", gamma=1.00)
+    kf_result = kf_linear(x=x, z=z)
+    logging.debug("Value of svm kernel rbf: %s", kf_result)
+    stop = timeit.default_timer()
+    logging.debug("Time consump by svm kernel rbf auto: %s", stop - start)
 
 
 def test_speed_tanh():
-    """Updating..."""
+    """Test speed (check) for np linear vs math linear kernel."""
     pass
+
+
+if __name__ == "__main__":
+    test_speed_rbf()
